d9f5110ca74948b17f608466f1c4fbb0d470df3b
Jambo <jambotoyou@gmail.com>
2022-11-14 15:39:10 +0400

    comm: commented CalculateArea method

--- ShapeAreas.Core/Shape.cs
+++ ShapeAreas.Core/Shape.cs
@@ -2,6 +2,10 @@
 {
 	public abstract class Shape
 	{
+		/// <summary>
+		/// Calculates the area for a shape
+		/// </summary>
+		/// <returns>Area value</returns>
 		public abstract double CalculateArea();
 	}
 }
